0918f39b79f05949ac1b83262bec682882cc0082
Matheus Eufrasio <matheus.eufrasio@XAXIM.PLANSUL>
2022-11-27 12:43:14 -0300

    Correção de textos

--- classes_objetos/abstract.php
+++ classes_objetos/abstract.php
@@ -1,7 +1,7 @@
 <div class="titulo">Classe Abstrata</div>
 
 <?php
-// A palavra reservada abstract serve para aplicar em classes é métodos.
+// A palavra reservada abstract serve para aplicar em classes e métodos.
 abstract class Abstrata { // Marcada como abstrata ela não pode ser instanciada.
   // O real uso de uma classe abstrata é para ter classes filhas.
     public abstract function metodo1(); // public pd ir antes de abstract se quiser.
@@ -41,6 +41,6 @@ $c->metodo1();
 $c->metodo3(); // Um método pode acessar outro, logo o 3 acessa o 2.
 print "Fim";
 
-/* É praticamente uma regra que na hierarquia de que as classes acima são
+/* É praticamente uma regra na hierarquia de que as classes acima são
 * mais genéricas e as que herdam são mais específicas.
 */
